5ac2868e6792682757a6c80c47470d4106f58b7d
Aaditya Chaudhari <115763672+aadi1720@users.noreply.github.com>
2023-11-28 23:48:37 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+def two_sum(nums, target):
+    # WRITE YOUR LOGIC HERE
+    pass
+
+def length_of_last_word(s):
+    # WRITE YOUR LOGIC HERE
+    pass
+
+def search_insert(nums, target):
+    # WRITE YOUR LOGIC HERE
+    pass
